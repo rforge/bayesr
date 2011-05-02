c9289c2f001e63cdab169b01559ebf45a5bb623e
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-02 06:41:00 +0000

    more forest
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R2BayesX/inst/sourcers.R
+++ pkg/R2BayesX/inst/sourcers.R
@@ -41,26 +41,26 @@ summary(ZambiaNutrition)
 ## forest health data
 # dat <- read.table("http://www.stat.uni-muenchen.de/~kneib/regressionsbuch/download/buche.raw", header = TRUE)
 ForestHealth <- list()
-ForestHealth$age <- dat$alter 
-ForestHealth$elevation <- dat$hoehe
-ForestHealth$inclination <- dat$hang 
-ForestHealth$soil <- dat$grund 
-ForestHealth$ph <- dat$ph 
-ForestHealth$canopy <- dat$schirm 
-ForestHealth$stand <- as.factor(dat$artkat)
-levels(ForestHealth$stand) <- c("deciduous", "mixed")
-ForestHealth$fertilized<- as.factor(dat$dueng)
-levels(ForestHealth$fertilization) <- c("yes", "no")
-ForestHealth$humus <- as.factor(dat$humus) 
-ForestHealth$moisture <- as.factor(dat$frische) 
-ForestHealth$saturation <- as.factor(dat$alkali) 
+ForestHealth$id <- dat$id 
 ForestHealth$year <- dat$jahr 
+ForestHealth$beech <- dat$buche
 ForestHealth$x <- dat$x
 ForestHealth$y <- dat$y
-ForestHealth$beech <- dat$buche
-ForestHealth$id <- dat$id 
+ForestHealth$age <- dat$alter 
+ForestHealth$canopy <- dat$schirm 
+ForestHealth$inclination <- dat$hang 
+ForestHealth$elevation <- dat$hoehe
+ForestHealth$soil <- dat$grund 
+ForestHealth$ph <- dat$ph 
+ForestHealth$moisture <- as.factor(dat$frische) 
+ForestHealth$saturation <- as.factor(dat$alkali) 
+ForestHealth$humus <- dat$humus
+ForestHealth$stand <- as.factor(dat$artkat)
+levels(ForestHealth$stand) <- c("deciduous", "mixed")
+ForestHealth$fertilized <- as.factor(dat$dueng)
+levels(ForestHealth$fertilized) <- c("yes", "no")
 ForestHealth <- as.data.frame(ForestHealth)
-## save(ForestHealth, file = "/home/nikolaus/svn/bayesr/pkg/R2BayesX/data/ForestHealth.rda")
+## save(ForestHealth, file = "/home/c403129/svn/bayesr/pkg/R2BayesX/data/ForestHealth.rda")
 
 
 
