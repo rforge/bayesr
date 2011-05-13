913c7b42e3455f6b43a6d54fbf640d2fe66098e3
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-13 10:34:39 +0000

    cprob
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R2BayesX/R/cprob.R
@@ -0,0 +1,15 @@
+cprob <-
+function(object, model = NULL, term = NULL, ...)
+{
+  object <- get.model(object, model)
+  if(length(object) > 1L) {
+    rval <- list()
+    for(i in 1L:length(object))
+      rval[[i]] <- attr(object[[i]]$effects[[term]], "contourprob")
+    } else rval <- attr(object[[1L]]$effects[[term]], "contourprob")
+  if(is.null(rval) || length(rval) < 1L)
+    rval <- NA
+
+  return(rval)
+}
+
