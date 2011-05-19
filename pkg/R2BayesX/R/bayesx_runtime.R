2ebdab4f816fe3ae59d750ebab1082a2ecf33268
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-19 13:47:25 +0000

    again file updates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@91 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R2BayesX/R/bayesx_runtime.R
@@ -0,0 +1,13 @@
+bayesx_runtime <- runtime <-
+function(x, model = 1L)
+{
+  runtime <- NULL
+  if(inherits(x, "bayesx")) {
+    runtime <- x[[model]]$bayesx.run$runtime
+      if(!is.null(runtime))
+        print(runtime)
+  }
+
+  return(invisible(runtime))
+}
+
