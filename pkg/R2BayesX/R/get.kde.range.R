71b4a0ec7c7fec797fbd81e23eaf8f2d5b4c4b60
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-23 05:16:21 +0000

    kde range
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@144 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R2BayesX/R/get.kde.range.R
@@ -0,0 +1,15 @@
+get.kde.range <- function(x, n = 100L, probs = c(0.01, 0.99)) 
+{
+  kde <- density(x, n = length(x), from = min(x), to = max(x))
+  f <- splinefun(x = kde$x, y = kde$y)
+  xgrid <- matrix(seq(min(x), max(x), length = n), ncol = 1L)
+  foo <- function(f, lower, x) integrate(f, lower = lower, upper = x)$value
+  xgridint <- apply(xgrid, 1L, foo, f = f, lower = min(x))
+  q01 <- xgrid[xgridint >= probs[1L]]
+  q01 <- q01[1L]
+  q99 <- xgrid[xgridint <= probs[2L]]
+  q99 <- q99[length(q99)]
+
+  return(c(q01, q99))
+}
+
