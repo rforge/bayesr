ddf678c852ae1c0079be2cd8f3e057afca6d4098
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-05 19:06:22 +0000

    pp
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@169 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R2BayesX/demo/forest.R
+++ pkg/R2BayesX/demo/forest.R
@@ -7,27 +7,24 @@ data("BeechBnd")
 ## estimate model without spatial effect
 fm1 <- bayesx(defoliation ~  stand + fertilized + 
   humus + moisture + alkali + ph + soil + 
-  s(age, bs = "ps") + s(inclination, bs = "ps") + 
-  s(canopy, bs = "ps") + s(year, bs = "ps") + 
-  s(elevation, bs = "ps"),
-  family = "cumlogit", method = "REML", data = ForestHealth)
+  sx(age) + sx(inclination) + sx(canopy) + 
+  sx(year) + sx(elevation), family = "cumlogit", 
+  method = "REML", data = ForestHealth)
 summary(fm1)
-plot(fm1, term = c("s(age)", "s(inclination)", 
-  "s(canopy)", "s(year)", "s(elevation)"))
+plot(fm1, term = c("sx(age)", "sx(inclination)", 
+  "sx(canopy)", "sx(year)", "sx(elevation)"))
 
 ## now include spatial effect
 ## warning: long runtime
 fm2 <- bayesx(defoliation ~  stand + fertilized + 
   humus + moisture + alkali + ph + soil + 
-  s(age, bs = "ps") + s(inclination, bs = "ps") + 
-  s(canopy, bs = "ps") + s(year, bs = "ps") + 
-  s(elevation, bs = "ps") + 
-  s(id, bs = "gk", xt = list(map = BeechBnd, full = TRUE)),
+  sx(age) + sx(inclination) + sx(canopy) + sx(year) + 
+  sx(elevation) + sx(id, bs = "gk", map = BeechBnd, full = TRUE),
   family = "cumlogit", method = "REML", data = ForestHealth)
 summary(fm2)
-plot(fm2, term = c("s(age)", "s(inclination)", 
-  "s(canopy)", "s(year)", "s(elevation)", "s(id)"),
+plot(fm2, term = c("sx(age)", "sx(inclination)", 
+  "sx(canopy)", "sx(year)", "sx(elevation)", "sx(id)"),
   map = BeechBnd, pos = "topleft")
 
 ## compare effects for elevation and inclination
-plot(c(fm1, fm2), term = c("s(elevation)", "s(inclination)"))
+plot(c(fm1, fm2), term = c("sx(elevation)", "sx(inclination)"))
--- pkg/R2BayesX/demo/zambia.R
+++ pkg/R2BayesX/demo/zambia.R
@@ -6,58 +6,57 @@ data("ZambiaBnd")
 
 ## estimate model
 zm1 <- bayesx(stunting ~ memployment + education + urban + gender + 
-  s(bmi, bs = "ps") + s(agechild, bs = "ps") +
-  s(district, bs = "mrf", xt = list(map = ZambiaBnd)) + r(district),
+  sx(bmi) + sx(agechild) + sx(district, bs = "mrf", map = ZambiaBnd) + r(district),
   iter = 12000, burnin = 2000, step = 10, data = ZambiaNutrition)
 
 ## summary statistics
 summary(zm1)
 
 ## visualising estimation results
-plot(zm1, term = "s(bmi)")
-plot(zm1, term = "s(agechild)")
+plot(zm1, term = "sx(bmi)")
+plot(zm1, term = "sx(agechild)")
 
-plot(zm1, term = "s(district)")
+plot(zm1, term = "sx(district)")
 plot(zm1, term = "r(district)")
 
-plot(zm1, term = c("s(district)", "r(district)"), 
+plot(zm1, term = c("sx(district)", "r(district)"), 
   map = ZambiaBnd, pos = "topleft")
 
 ## customizing graphics
-plot(zm1, term = "s(bmi)", resid = TRUE, cex = 0.1)
-plot(zm1, term = "s(agechild)", resid = TRUE, cex = 0.1)
+plot(zm1, term = "sx(bmi)", resid = TRUE, cex = 0.1)
+plot(zm1, term = "sx(agechild)", resid = TRUE, cex = 0.1)
 
-plot(zm1, term = "s(bmi)", main = "Mother body mass index", 
+plot(zm1, term = "sx(bmi)", main = "Mother body mass index", 
   xlab = "",ylab = "")
-plot(zm1, term = "s(bmi)", main = "Mother body mass index", 
+plot(zm1, term = "sx(bmi)", main = "Mother body mass index", 
   xlab = "",ylab = "", ylim = c(-0.8, 0.6))
-plot(zm1, term = "s(bmi)", main = "Mother body mass index", 
+plot(zm1, term = "sx(bmi)", main = "Mother body mass index", 
   xlab = "",ylab = "", ylim = c(-0.8, 0.6), rug = FALSE)
-plot(zm1, term = "s(bmi)", main = "Mother body mass index", 
+plot(zm1, term = "sx(bmi)", main = "Mother body mass index", 
   xlab = "",ylab = "", ylim = c(-0.8, 0.6), rug = FALSE, 
   col.poly = NA, lwd = 1, lty = 1)
-plot(zm1, term = "s(bmi)", main = "Mother body mass index", 
+plot(zm1, term = "sx(bmi)", main = "Mother body mass index", 
   xlab = "", ylab = "", ylim = c(-0.8, 0.6), rug = FALSE, 
   col.poly = NA, lwd = 1, lty = c(3, 1, 2, 2, 1))
 
-plot(zm1, term = "s(district)", map = ZambiaBnd, pos = "topleft")
+plot(zm1, term = "sx(district)", map = ZambiaBnd, pos = "topleft")
 plot(zm1, term = "r(district)", map = ZambiaBnd, pos = "topleft")
 plot(zm1, term = "r(district)", map = ZambiaBnd, pos = "topleft", 
   density = 30, swap = TRUE)
-plot(zm1, term = "s(district)", map = ZambiaBnd, pos = "topleft", 
+plot(zm1, term = "sx(district)", map = ZambiaBnd, pos = "topleft", 
   names = TRUE)
-plot(zm1, term = "s(district)", map = ZambiaBnd, pos = "topleft", 
+plot(zm1, term = "sx(district)", map = ZambiaBnd, pos = "topleft", 
   names = TRUE, cex.names = 0.8, cex.legend = 0.8)
-plot(zm1, term = "s(district)", map = ZambiaBnd, pos = "topleft", 
+plot(zm1, term = "sx(district)", map = ZambiaBnd, pos = "topleft", 
   range = c(-0.2, 0.2))
-plot(zm1, term = "s(district)", map = ZambiaBnd, pos = "topleft", 
+plot(zm1, term = "sx(district)", map = ZambiaBnd, pos = "topleft", 
   c.select = "pcat95", at = c(-1, 0, 1), ncol = 3)
-plot(zm1, term = "s(district)", map = ZambiaBnd, pos = "topleft", 
+plot(zm1, term = "sx(district)", map = ZambiaBnd, pos = "topleft", 
   c.select = "pcat80", at = c(-1, 0, 1), ncol = 3)
 
 op <- par(no.readonly = TRUE)
 par(mfrow = c(1, 2))
-plot(zm1, term = "s(district)", map = ZambiaBnd, pos = "topleft", 
+plot(zm1, term = "sx(district)", map = ZambiaBnd, pos = "topleft", 
   main = "Structured spatial effect", names = TRUE)
 plot(zm1, term = "r(district)", map = ZambiaBnd, pos = "topleft", 
   density = 40, main = "Unstructured spatial effect", names = TRUE)
@@ -70,47 +69,46 @@ plot(zm1, which = "intcpt-samples")
 plot(zm1, term = c("memployment", "education"), 
   which = "coef-samples", ask = TRUE)
 
-plot(zm1, term = "s(bmi)", which = "coef-samples")
-plot(zm1, term = "s(bmi)", which = "coef-samples", acf = TRUE)
+plot(zm1, term = "sx(bmi)", which = "coef-samples")
+plot(zm1, term = "sx(bmi)", which = "coef-samples", acf = TRUE)
 
-plot(zm1, term = "s(bmi)", which = "var-samples")
-plot(zm1, term = "s(bmi)", which = "var-samples", acf = TRUE)
-plot(zm1, term = "s(bmi)", which = "var-samples", acf = TRUE, lag.max = 400)
+plot(zm1, term = "sx(bmi)", which = "var-samples")
+plot(zm1, term = "sx(bmi)", which = "var-samples", acf = TRUE)
+plot(zm1, term = "sx(bmi)", which = "var-samples", acf = TRUE, lag.max = 400)
 
-plot(zm1, term = "s(agechild)", which = "var-samples")
-plot(zm1, term = "s(district)", which = "var-samples")
+plot(zm1, term = "sx(agechild)", which = "var-samples")
+plot(zm1, term = "sx(district)", which = "var-samples")
 plot(zm1, term = "r(district)", which = "var-samples")
 
 ## sensitivity analysis 
 zm2 <- bayesx(stunting ~ memployment + education + urban + gender + 
-  s(bmi, bs = "ps", xt = list(a = 0.00001, b = 0.00001)) +
-  s(agechild, bs = "ps", xt = list(a = 0.00001, b = 0.00001)) +
-  s(district, bs = "mrf", xt = list(map = ZambiaBnd, a = 0.00001, b = 0.00001)) +
+  sx(bmi, a = 0.00001, b = 0.00001) +
+  sx(agechild, a = 0.00001, b = 0.00001) +
+  sx(district, bs = "mrf", map = ZambiaBnd, a = 0.00001, b = 0.00001) +
   r(district, xt = list(a = 0.00001, b = 0.00001)),
   iter = 12000, burnin = 2000, step = 10, data = ZambiaNutrition)
 
 zm3 <- bayesx(stunting ~ memployment + education + urban + gender +
-  s(bmi, bs = "ps", xt = list(a = 0.005, b = 0.005)) +
-  s(agechild, bs = "ps", xt = list(a = 0.005, b = 0.005)) +
-  s(district, bs = "mrf", xt = list(map = ZambiaBnd, a = 0.005, b = 0.005)) +
+  sx(bmi, a = 0.005, b = 0.005) +
+  sx(agechild, a = 0.005, b = 0.005) +
+  sx(district, bs = "mrf", map = ZambiaBnd, a = 0.005, b = 0.005) +
   r(district, xt = list(a = 0.005, b = 0.005)),
   iter = 12000, burnin = 2000, step = 10, data = ZambiaNutrition)
 
 zm4 <- bayesx(stunting ~ memployment + education + urban + gender +
-  s(bmi, bs= "ps", xt = list(a = 0.00005, b = 0.00005)) +
-  s(agechild, bs = "ps", xt = list(a = 0.00005, b = 0.00005)) +
-  s(district, bs = "mrf", xt = list(map = ZambiaBnd, a = 0.00005, b = 0.00005)) +
+  sx(bmi, a = 0.00005, b = 0.00005) +
+  sx(agechild, a = 0.00005, b = 0.00005) +
+  sx(district, bs = "mrf", map = ZambiaBnd, a = 0.00005, b = 0.00005) +
   r(district, xt = list(a = 0.00005, b = 0.00005)),
   iter = 12000, burnin = 2000, step = 10, data = ZambiaNutrition)
 
-plot(c(zm1, zm2, zm3, zm4), term = "s(bmi)")
-plot(c(zm1, zm2, zm3, zm4), term = "s(agechild)")
+plot(c(zm1, zm2, zm3, zm4), term = "sx(bmi)")
+plot(c(zm1, zm2, zm3, zm4), term = "sx(agechild)")
 
 
 ## same model with REML
 zm5 <- bayesx(stunting ~ memployment + education + urban + gender + 
-  s(bmi, bs = "ps") + s(agechild, bs = "ps") +
-  s(district, bs = "mrf", xt = list(map = ZambiaBnd)) + r(district),
+  sx(bmi) + sx(agechild) + sx(district, bs = "mrf", map = ZambiaBnd) + r(district),
   method = "REML", data = ZambiaNutrition)
 summary(zm5)
 plot(zm5, map = ZambiaBnd)
@@ -118,8 +116,6 @@ plot(zm5, map = ZambiaBnd)
 
 ## now use the stepwise algorithm
 zm6 <- bayesx(stunting ~ memployment + education + urban + gender + 
-  s(bmi, bs = "ps") + s(agechild, bs = "ps") +
-  s(district, bs = "mrf", xt = list(map = ZambiaBnd)) + r(district),
-  iter = NULL, burnin = NULL, step = NULL, method = "STEP", 
-  data = ZambiaNutrition, dir.rm = FALSE, CI = NULL, bootstrapsamples = NULL)
+  sx(bmi) + sx(agechild) + sx(district, bs = "mrf", map = ZambiaBnd) + r(district),
+  method = "STEP", data = ZambiaNutrition)
 summary(zm6)
