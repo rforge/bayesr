4a33260acd27ad055caf47cb924a93010f215070
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-10 16:08:57 +0000

    ok hope it works
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@187 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesR/R/print.gibbs.R
+++
@@ -1,9 +0,0 @@
-print.gibbs <- function(x,...)
-	{
-	object <- x
-	cat("Call:\n")
-	print(object$call)
-	cat("-------------------------------------------------------------------\n")
-	cat("DIC =", object$DIC$DIC, " pd =", object$DIC$pd, " n =", object$N, "\n")
-	cat("Iterations =", object$iterations, " burnin =", object$burnin, " thinning =", object$thinning, "\n")
-	}
--- pkg/BayesR/R/rtruncated.R
+++
@@ -1,6 +0,0 @@
-`rtruncated` <-
-function (n, lo, hi, pf, qf, ...)
-{ 
-qf(pf(lo, ...) + runif(n) * (pf(hi, ...) - pf(lo, ...)), ...)
-}
-
