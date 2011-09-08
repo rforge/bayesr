625a2b39620c68246edc6964c04cf1f117ef2420
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-08 11:52:17 +0000

    fixed f2int.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@229 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R2BayesX/R/f2int.R
+++ pkg/R2BayesX/R/f2int.R
@@ -6,7 +6,9 @@ function(x)
   if(any(is.na(as.numeric(as.character(x)))))
     if(is.factor(x))
       levels(x) <- 1:nlevels(x)
-  x <- as.integer(as.numeric(as.character(x))) - 1
+  x <- as.integer(as.numeric(as.character(x))) - 1L
+  if(min(x) < 0)
+    x <- x + min(x)
   options(warn = warn)
 
   return(x)
