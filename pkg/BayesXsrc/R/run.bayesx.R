720a4875b215c9b8bde0dc5c6ca7f19a5ee8ff3d
dadler <dadler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-21 11:29:07 +0000

    system.file(package=..) pointed to wrong package name
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@250 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesXsrc/R/run.bayesx.R
+++ pkg/BayesXsrc/R/run.bayesx.R
@@ -1,9 +1,9 @@
 run.bayesx <- function(wait = TRUE, ...) 
 {
   if (.Platform$OS.type == "windows") {
-    cmd <- shQuote(system.file(package="bayesxsrc", "libs", .Platform$r_arch, "BayesX.exe"))
+    cmd <- shQuote(system.file(package="BayesXsrc", "libs", .Platform$r_arch, "BayesX.exe"))
   } else {
-    fn <- shQuote(system.file(package="bayesxsrc", "libs", .Platform$r_arch, "BayesX"))
+    fn <- shQuote(system.file(package="BayesXsrc", "libs", .Platform$r_arch, "BayesX"))
     cmd <- paste(shQuote(file.path(R.home(),"bin","R")), "CMD", fn)
   }
   system(cmd, wait=wait, ...)
