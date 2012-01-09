b40c698c5e7850d5e4cc3aba72376c87fe9b0a58
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-09 13:53:13 +0000

    now with new run.bayesx, need to adapt other functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@260 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R2BayesX/R/run.bayesx.R
+++ pkg/R2BayesX/R/run.bayesx.R
@@ -1,54 +1,59 @@
 run.bayesx <-
-function(dir, prg.name = "bayesx.estim.input.prg", 
-  verbose = TRUE, bin = NULL)
+function(prg = NULL, verbose = TRUE, ...)
 {
-  if(is.null(bin)) bin <- findBayesX()
-  ok <- check.install.bayesx(bin, verbose = FALSE) 
-  if(is.null(ok))
-    stop("please install BayesX or verify bin path, also see function install.bayesx()")
-  fileo <- getwd()
-  setwd(dir)
-  output <- file.exists("output")
-  temp <- file.exists("temp")
-  win <- FALSE
-  ptm <- proc.time()
-  if(!verbose) {
-    if(.Platform$OS.type == "unix") {
-      log <- try(system(paste(ok, " ", dir, "/", prg.name, 
-        " > ", dir, "/bayesx.log", sep = ""), intern = FALSE))
-    } else {
-      log <- try(system(paste(ok, " ", dir, "/", prg.name, sep = ""), 
-        intern = TRUE, show.output.on.console = FALSE))
-      win <- TRUE
-    }
-  } else log <- try(system(paste(ok, " ", dir, "/", prg.name, sep = ""), intern = FALSE))
-	if(log != 0 && !win)
-    warning("problem processing BayesX (maybe verify bin path)!")
-  if(!verbose && .Platform$OS.type == "unix")	
-    log <- readLines(paste(dir, "/bayesx.log", sep = ""))
-  now <- proc.time()
-  runtime <- now - ptm
-  runtime <- runtime
-  if(verbose)
-    cat("Total run time was:", runtime[3L], "sec\n")
-  if(!output)
-    output <- file.exists("output")
-  if(output)
-    try(suppressWarnings(file.remove("output")), silent = TRUE)
-  if(!output)
-    output <- file.exists("temp")
-  if(output)
-    try(suppressWarnings(file.remove("temp")), silent = TRUE)
-  try(setwd(fileo), silent = TRUE)
-  logerror <- FALSE
-  for(logline in log)
-    if(grepl("ERROR", logline))
-      logerror <- TRUE
-  if(logerror) {
-    warning(paste("an error occurred during runtime of BayesX, please check the logfile,", 
-      "see the help site of function bayesx_logfile()!"))
+  os.win <- .Platform$OS.type == "windows"
+  if(os.win) {
+    bin <- shQuote(system.file(package = "BayesXsrc", "libs", .Platform$r_arch, "BayesX.exe"))
+  } else {
+    bin <- shQuote(system.file(package = "BayesXsrc", "libs", .Platform$r_arch, "BayesX"))
   }
-
-  return(invisible(list(log = log, runtime = runtime)))
+  if(is.null(prg)) {
+    if(!os.type)
+      bin <- paste(shQuote(file.path(R.home(),"bin","R")), "CMD", bin)
+    system(bin, ...)
+    return(invisible(NULL))
+  } else {
+    fileo <- getwd()
+    prg <- path.expand(prg)
+    dir <- dirname(prg)
+    prg.name <- basename(prg)
+    setwd(dir)
+    output <- file.exists("output")
+    temp <- file.exists("temp")
+    ptm <- proc.time()
+    if(!verbose) {
+      if(os.win) {
+        log <- try(system(paste(bin, " ", dir, "/", prg.name, sep = ""), 
+          intern = TRUE, show.output.on.console = FALSE, ...))
+      } else {
+        log <- try(system(paste(bin, " ", dir, "/", prg.name, 
+          " > ", dir, "/bayesx.log", sep = ""), intern = FALSE, ...))
+      }
+    } else log <- try(system(paste(bin, " ", dir, "/", prg.name, sep = ""), intern = FALSE, ...))
+	  if(log != 0 && !os.win)
+      warning("problem processing BayesX!")
+    if(!verbose && .Platform$OS.type == "unix")	
+      log <- readLines(paste(dir, "/bayesx.log", sep = ""))
+    now <- proc.time()
+    runtime <- now - ptm
+    runtime <- runtime
+    if(verbose)
+      cat("Total run time was:", runtime[3L], "sec\n")
+    if(!output)
+      output <- file.exists("output")
+    if(output)
+      try(suppressWarnings(file.remove("output")), silent = TRUE)
+    if(!output)
+      output <- file.exists("temp")
+    if(output)
+      try(suppressWarnings(file.remove("temp")), silent = TRUE)
+    try(setwd(fileo), silent = TRUE)
+    logerror <- FALSE
+    for(logline in log)
+      if(grepl("ERROR", logline))
+        logerror <- TRUE
+    if(logerror)
+      warning("an error occurred during runtime of BayesX, please check the BayesX logfile!")
+    return(invisible(list(log = log, runtime = runtime)))
+  }
 }
-
