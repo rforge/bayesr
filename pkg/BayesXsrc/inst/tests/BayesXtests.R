f94608420a7bb672957300feabeac8f3330b2774
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-09 10:49:04 +0000

    creating real tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@257 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesXsrc/inst/tests/BayesXtests.R
+++
@@ -1,9 +0,0 @@
-## BayesX testing
-testfiles <- c("mcmc.prg", "reml.prg", "step.prg", "BayesX-tests.R", "data.raw")
-
-mcmc <- run.bayesx("mcmc.prg")
-reml <- run.bayesx("reml.prg")
-step <- run.bayesx("step.prg")
-
-files <- list.files()
-file.remove(files[!files %in% testfiles])
