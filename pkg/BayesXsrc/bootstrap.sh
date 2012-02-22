a33ee8b0959a8a993efec75227a0424a9dc4dfab
dadler <dadler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-22 23:25:12 +0000

    quick fixes of bootstrap (thx for Z ;-)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@269 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesXsrc/bootstrap.sh
+++ pkg/BayesXsrc/bootstrap.sh
@@ -1,12 +1,12 @@
 R=http://svn.gwdg.de/svn/bayesx/trunk
 U=guest
 P=guest
-DIRS="adaptiv alex andrea bib dag export_type.h graph leyre main.cpp mcmc psplines samson structadd values.h"
+DIRS="adaptiv alex andrea bib dag graph leyre mcmc psplines samson structadd"
 mkdir -p src/bayesxsrc
 cd src/bayesxsrc
-#for i in $DIRS ; do
-#  svn co --username "${U}" --password "${P}" $R/$i $i
-#done
+for i in $DIRS ; do
+  svn co --username "${U}" --password "${P}" $R/$i $i
+done
 FILES="export_type.h main.cpp values.h"
 for i in $FILES ; do
   svn export --username "${U}" --password "${P}" $R/$i $i
