1e7cf9293719906f427e9549b6062f077c46a43d
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-08 17:04:03 +0000

    Renamed bootstrap.sh to bootstrap-devel.sh as this checks out the most recent revision from the BayesX SVN.
    Added bootstrap-release.sh which checks out revision 948 which corresponds
    to the release of BayesX 2.1 - the current stable release version.
    
    Both .sh scripts have been slightly edited to be even more readable.
    
    Removed old bootstrap-cvs.sh as this is not needed anymore.
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@356 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/BayesXsrc/bootstrap-devel.sh
@@ -0,0 +1,14 @@
+REPOS=http://svn.gwdg.de/svn/bayesx/trunk
+USER=guest
+PASSWD=guest
+DIRS="adaptiv alex andrea bib dag graph leyre mcmc psplines samson structadd"
+FILES="export_type.h main.cpp values.h"
+mkdir -p src/bayesxsrc
+cd src/bayesxsrc
+for i in $DIRS ; do
+  svn checkout --username "${USER}" --password "${PASSWD}" $REPOS/$i $i
+done
+for i in $FILES ; do
+  svn export --username "${USER}" --password "${PASSWD}" $REPOS/$i $i
+done
+
