a62b104248b37f26c9acb3326e4040ce3a3cc688
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-13 13:47:33 +0000

    changed the warning to an error if predict() is called with newdata
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@394 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R2BayesX/R/predict.bayesx.R
+++ pkg/R2BayesX/R/predict.bayesx.R
@@ -2,7 +2,7 @@ predict.bayesx <-
 function(object, newdata, model = NULL, term = NULL, ...) 
 {
   if(!missing(newdata))
-    warning("out of sample prediction currently not available, using fitted values instead!")
+    stop("out of sample prediction currently not available")
 
   return(fitted.bayesx(object, model, term, ...))
 }
