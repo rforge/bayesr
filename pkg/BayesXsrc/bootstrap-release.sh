4d53b4bb71496a12fce4f6696e63174c5046b8cc
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-06-14 05:42:13 +0000

    BayesXsrc prepared for new release of BayesXsrc 2.1-2
    - DESCRIPTION/NEWS updated accordingly
    - needs bootstrap-release.sh with BayesX/r953 (for quasi-release version
      of BayesX itself)
    - Makefiles, however, need to be taken from BayesXsrc/r457 because the
      most recent versions only run with bootstrap-devel.sh...
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@553 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesXsrc/bootstrap-release.sh
+++ pkg/BayesXsrc/bootstrap-release.sh
@@ -6,11 +6,9 @@ FILES="export_type.h main.cpp values.h"
 mkdir -p src/bayesxsrc
 cd src/bayesxsrc
 for i in $DIRS ; do
-  svn checkout --username "${USER}" --password "${PASSWD}" $REPOS/$i $i
-  # svn checkout --revision 953 --username "${USER}" --password "${PASSWD}" $REPOS/$i $i
+  svn checkout --revision 953 --username "${USER}" --password "${PASSWD}" $REPOS/$i $i
 done
 for i in $FILES ; do
-  svn export --username "${USER}" --password "${PASSWD}" $REPOS/$i $i
-  # svn export --revision 953 --username "${USER}" --password "${PASSWD}" $REPOS/$i $i
+  svn export --revision 953 --username "${USER}" --password "${PASSWD}" $REPOS/$i $i
 done
 
