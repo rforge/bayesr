9fd5ff516369206c904698011ff49a9826f33737
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-17 13:25:48 +0000

    small changes only
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@724 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesR/tests/ZambiaNutrition.R
+++ pkg/BayesR/tests/ZambiaNutrition.R
@@ -5,14 +5,15 @@ data("ZambiaBnd", package = "R2BayesX")
 
 K <- neighbormatrix(ZambiaBnd, id = ZambiaNutrition$district)
 
-ZambiaNutrition$district <- as.factor(ZambiaNutrition$district)
+ZambiaNutrition$district <- ZambiaNutrition$district2 <- as.factor(ZambiaNutrition$district)
 
 f <- stunting ~ memployment + urban + gender + meducation + s(mbmi) +
   s(agechild) + s(district, bs = "mrf", xt = list("penalty" = K)) +
   s(district, bs = "re")
 
 b0 <- bayesr(f, family = gaussian, data = ZambiaNutrition, engine = "JAGS")
-b1 <- bayesr(f, family = gaussian, data = ZambiaNutrition, engine = "IWLS")
+b1 <- bayesr(f, f, family = gaussian, data = ZambiaNutrition, engine = "IWLS",
+  n.iter = 1200, burnin = 200, thin = 1)
 
 f <- stunting ~ -1 + memployment + urban + gender + meducation + sx(mbmi) +
   sx(agechild) + sx(district, bs = "mrf", map = ZambiaBnd) +
