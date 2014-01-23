8929189ec3469565616fd0a52eef224443f8700d
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-23 13:41:51 +0000

    added sp+bnd functionality of old BayesX project
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@814 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesR/R/vis.R
+++ pkg/BayesR/R/vis.R
@@ -1481,6 +1481,38 @@ df2m <- function(x)
 }
 
 
+find.limits <- function(map, mar.min = 2, ...)
+{
+  if(!is.list(map))
+    stop("argument map must be a list() of matrix polygons!")
+  n <- length(map)
+  myrange <- function(x, c.select = 1L, ...) {
+    return(na.omit(x[, c.select], ...))
+  }
+  xlim <- range(unlist(lapply(map, myrange, c.select = 1L, ...)))
+  ylim <- range(unlist(lapply(map, myrange, c.select = 2L, ...)))
+  mar <- NULL
+  asp <- attr(map, "asp")
+  if(is.null(asp))
+    asp <- (diff(ylim) / diff(xlim)) / cos((mean(ylim) * pi) / 180)
+  if(!is.null(height2width <- attr(map, "height2width"))) {
+    height2width <- height2width * 0.8
+    if(!is.null(mar.min)) {
+      if(height2width > 1) {
+        side <- 17.5 * (1 - 1/height2width) + mar.min / height2width
+        mar <- c(mar.min, side, mar.min, side)
+      }
+      else {
+        top <- 17.5  * (1 - height2width) + mar.min * height2width
+        mar <- c(top, mar.min, top, mar.min)
+      }
+    }
+  }
+
+  return(list(ylim = ylim, xlim = xlim, mar = mar, asp = asp))
+}
+
+
 set.plot2d.specs <- function(nc, args, col.lines, is.bayesx)
 {
   lwd <- args$lwd
@@ -1574,5 +1606,15 @@ interp2 <- function(x, y, z, xo = NULL, yo = NULL, grid = 30,
   }
 
   return(matrix(fit, grid, grid))
+}
+
+
+x2int <- function(x) 
+{
+  warn <- getOption("warn")
+  options(warn = -1)
+  rval <- as.integer(as.numeric(as.character(x)))
+  options("warn" = warn)
+  rval
 }
 
