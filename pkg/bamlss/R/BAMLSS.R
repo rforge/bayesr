d95d9573942cbf43c1bf3752a35fe193d0dd84f5
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-22 19:18:17 +0000

    added Viola Obermeier's smooth.construct() method for lag function estimation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@1112 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/R/BAMLSS.R
+++ pkg/bamlss/R/BAMLSS.R
@@ -2127,6 +2127,41 @@ Predict.matrix.kriging.smooth <- function(object, data)
 }
 
 
+## Smooth constructor for lag function.
+## C Viola Obermeier.
+smooth.construct.fdl.smooth.spec <- function(object, data, knots)
+{
+  ## Modify object so that it's fitted as a p-spline signal regression term.
+  object$bs <- "ps"
+  object <- smooth.construct.ps.smooth.spec(object, data, knots)
+
+  if(!is.null(object$xt$fullrankpen) && object$xt$fullrankpen){
+    ## Add ridge penalty to first <order of B-spline>+1 (=m+2) basis functions.
+    ## With same variance as difference penalty: penalty = lambda * coef' (DiffPen + RidgePen) coef.
+    object$S[[1]][cbind(1:(object$m[1]+2), 1:(object$m[1]+2))] <- object$S[[1]][cbind(1:(object$m[1]+2), 1:(object$m[1]+2))] + 1
+    object$rank <- min(object$bs.dim, object$rank + object$m[1]+2)
+  }
+  if(!is.null(object$xt$ridge) && object$xt$ridge){
+    ## Add ridge penalty to first <order of B-spline>+1 (=m+2) basis functions
+    ## penalty = coef' (lambda_1*DiffPen + lambda_2*RidgePen) coef.
+    object$S[[2]] <- matrix(0, object$bs.dim, object$bs.dim)
+    object$S[[2]][cbind(1:(object$m[1]+2), 1:(object$m[1]+2))] <- 1
+    object$rank <- c(object$rank, object$m[1]+2)
+  }
+  if(!is.null(object$xt$constrain) && object$xt$constrain){
+    ## Optionally one can constrain the last lag coefficient to be zero,
+    ## not recommended as we favor a soft, data-driven shrinkage to a hard constraint!
+    ## Constrain to end in zero (i.e (X%*%coefficients)[1] == 0).
+    ## --> Constraint matric C = X[1,]
+    object$C <- matrix(object$X[1,],nrow=1)
+    object$C <- structure(object$C, always.apply=TRUE)
+  }
+
+  return(object)
+}
+
+
+
 ###################
 ## (6) Plotting. ##
 ###################
