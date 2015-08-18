004dfb23084024d8ee8e484dc5b529dc30d3bc49
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-18 05:56:23 +0000

    added multinomial
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@1389 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/R/samplers.R
+++ pkg/bamlss/R/samplers.R
@@ -692,10 +692,10 @@ gmcmc_sm.iwls <- function(family, theta, id, prior, eta, response, data, ...)
   peta <- family$map2par(eta)
 
   ## Compute weights.
-  weights <- family$hess[[id[1]]](response, peta)
+  weights <- family$hess[[id[1]]](response, peta, id = id)
 
   ## Score.
-  score <- family$score[[id[1]]](response, peta)
+  score <- family$score[[id[1]]](response, peta, id = id)
 
   ## Compute working observations.
   z <- eta[[id[1]]] + 1 / weights * score
@@ -750,10 +750,10 @@ gmcmc_sm.iwls <- function(family, theta, id, prior, eta, response, data, ...)
   pibetaprop <- family$loglik(response, peta)
 
   ## Compute new weights
-  weights <- family$hess[[id[1]]](response, peta)
+  weights <- family$hess[[id[1]]](response, peta, id = id)
 
   ## New score.
-  score <- family$score[[id[1]]](response, peta)
+  score <- family$score[[id[1]]](response, peta, id = id)
 
   ## New working observations.
   z <- eta[[id[1]]] + 1 / weights * score
