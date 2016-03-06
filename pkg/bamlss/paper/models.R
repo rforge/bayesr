a090c04eea7b541acc7046dc34fd069dc435239d
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-03-06 18:13:57 +0000

    pap
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@1680 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/paper/models.R
+++ pkg/bamlss/paper/models.R
@@ -55,7 +55,7 @@ if(!file.exists("firemodel.rda") & FALSE) {
 
   firemodel <- bamlss(f, data = LondonFire, family = "cox",
     subdivisions = 25, nu = 0.01, maxit = 1000,
-    n.iter = 4000, burnin = 2000, thin = 10, cores = 8)
+    n.iter = 60000, burnin = 20000, thin = 200, cores = 6, df = 10)
 
   save(firemodel, file = "firemodel.rda")
 
