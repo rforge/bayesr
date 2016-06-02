805142491cb1935fea1cc4bc1f653db7552d0b6e
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-06-02 10:14:51 +0000

    modified project website
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@1846 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,6 +43,22 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
+<h3>Bayesian Additive Models for Location Scale and Shape (and Beyond)</h3>
+The R package bamlss is a general tool for complex Bayesian regression modeling
+with structured additive predictors based on Markov chain Monte Carlo simulation.
+The design of this package substantially focuses on maximum flexibility and easy
+integration of new code and/or standalone systems (e.g., JAGS and BayesX).
+<p> Supplemtary files: </p>
+<ul>
+<li> The <a href="DAGStat2016.pdf">DAGStat 2016 slides</a>,</li>
+<li> Slides from a talk at <a href="Linz2015.pdf">JKU Linz 2015</a>.</li>
+</ul>
+
+<h3>The R interface to BayesX</h3>
+The R package R2BayesX provides an interface to BayesX, a software tool for estimating structured
+additive regression models (STAR).  With R2BayesX, STAR models can be conveniently specified using
+Rs formula language (with some extended terms), fitted using the BayesX binary, represented in R
+with objects of suitable classes, and finally printed/summarized/plotted.
 <p> Supplemtary files for the interface package R2BayesX: </p>
 <ul>
 <li> The <a href="https://www.jstatsoft.org/article/view/v063i21" target="_new">R2BayesX paper</a>,</li>
@@ -51,6 +67,10 @@ echo $contents; } ?>
 <li> The <a href="zambia.R">ZambiaNutrition demo</a> is containing example code for MCMC and STEP estimation,</li>
 <li> and the <a href="forest.R">ForestHealth demo</a> illustrates estimation using REML.</li>
 </ul>
+
+<h3>The R Package Distribution of the BayesX C++ Sources</h3>
+The R package BayesXsrc provides the BayesX command line tool for easy installation.
+A convenient R interface is provided in package R2BayesX.
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
