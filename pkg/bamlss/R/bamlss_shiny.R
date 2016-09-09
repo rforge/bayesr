9c582d1fe46da6c205e17252cf75eebb16fc0205
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-09 05:35:12 +0000

    fun with shiny
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@2009 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/R/bamlss_shiny.R
+++ pkg/bamlss/R/bamlss_shiny.R
@@ -25,7 +25,7 @@ bamlss_shiny_ui <- function(...) {
          uiOutput("select_model"),
          actionButton("load_model", label = "Load model."),
          tags$hr(),
-         uiOutput("select_variables"),
+         uiOutput("select_terms"),
          uiOutput("data_select")
        )
      ),
@@ -35,7 +35,7 @@ bamlss_shiny_ui <- function(...) {
        uiOutput("plot_type_button"),
        uiOutput("plot_parameters"),
        uiOutput("plot_button"),
-       plotOutput("the_plot", width = '60%'),
+       plotOutput("the_plot", height = "800px", width = "600px"),
        tags$br()
      )
    ))
@@ -72,25 +72,37 @@ bamlss_shiny_server <- function(input, output, session)
   observeEvent(input$load_model, {
     if(file.exists("predictions/pred.Rda"))
       file.remove("predictions/pred.Rda")
-    if((input$selected_model != "") | input$selected_model != "No 'bamlss' model available!") {
+    if((input$selected_model != "") & !grepl("model available", input$selected_model)) {
       m <- get(input$selected_model, envir = .GlobalEnv)
-      mf <- na.omit(model.frame(m))
-      output$select_variables <- renderUI({
-        selectInput('variables_selected', 'Select covariates for visualization.',
-          names(mf), multiple = TRUE, selectize = TRUE)
+      mf <- model.frame(m)
+      tn <- lapply(formula(m), function(x) {
+        all.labels.formula(x$formula)
+      })
+      tn <- unique(do.call("c", tn))
+      output$select_terms <- renderUI({
+        selectInput('terms_selected', 'Select terms for visualization.',
+          tn, multiple = TRUE, selectize = TRUE)
       })
       output$data_select <- renderUI({
-        if(!is.null(input$variables_selected)) {
-          sliders <- lapply(input$variables_selected, function(i) {
+        if(!is.null(input$terms_selected)) {
+          vars <- NULL
+          for(j in input$terms_selected)
+            vars <- c(vars, all.vars(as.formula(paste("~", j))))
+          vars <- unique(vars)
+          for(j in seq_along(vars)) {
+            if(!any(vars[j] %in% names(mf)))
+              vars[j] <- grep(vars[j], names(mf), fixed = TRUE, value = TRUE)
+          }
+          sliders <- lapply(vars, function(i) {
             vn <- paste("var_", i, sep = "")
             if(!is.factor(mf[[i]])) {
               x <- pretty(mf[[i]])
-              sliderInput(vn, i, min = min(x), max = max(x), value = range(x))
+              return(sliderInput(vn, i, min = min(x), max = max(x), value = range(x)))
             } else {
-              selectInput(vn, i, levels(mf[[i]]))
+              return(selectInput(vn, i, levels(mf[[i]])))
             }
           })
-          rval <- do.call(tagList, c(list(tags$b("Choose covariate values.")), sliders))
+          rval <- do.call(tagList, c(list(tags$b("Choose data range.")), sliders))
         } else rval <- NULL
         rval
       })
@@ -98,9 +110,11 @@ bamlss_shiny_server <- function(input, output, session)
   })
 
   output$predict <- renderUI({
-    if(!is.null(input$variables_selected)) {
+    if(!is.null(input$terms_selected)) {
       rval <- tagList(
         numericInput("ngrid", "Size of grid.", 30, min = 1, max = 1000),
+        selectInput('predict_fun', 'Select function applied on samples.',
+          c("None", "Mean", "Median", "Sd", "Var", "95% CI", "99% CI", "c95"), selected = "c95"),
         actionButton("predict_model", label = "Predict."),
         tags$hr()
       )
@@ -109,11 +123,19 @@ bamlss_shiny_server <- function(input, output, session)
   })
 
   observeEvent(input$predict_model, {
-    if(!is.null(input$variables_selected)) {
+    if(!is.null(input$terms_selected)) {
       m <- get(input$selected_model, envir = .GlobalEnv)
       mf <- na.omit(model.frame(m))
+      vars <- NULL
+      for(j in input$terms_selected)
+        vars <- c(vars, all.vars(as.formula(paste("~", j))))
+      vars <- unique(vars)
+      for(j in seq_along(vars)) {
+        if(!any(vars[j] %in% names(mf)))
+          vars[j] <- grep(vars[j], names(mf), fixed = TRUE, value = TRUE)
+      }
       nd <- list()
-      for(i in input$variables_selected) {
+      for(i in vars) {
         vn <- paste("var_", i, sep = "")
         if(is.character(input[[vn]])) {
           nd[[i]] <- factor(input[[vn]], levels = levels(mf[[i]]))
@@ -122,9 +144,18 @@ bamlss_shiny_server <- function(input, output, session)
         }
       }
       nd <- expand.grid(nd)
+      FUN <- switch(input$predict_fun,
+        "None" = function(x) { x },
+        "Mean" = function(x) { mean(x, na.rm = TRUE) },
+        "Median" = function(x) { median(x, na.rm = TRUE) },
+        "Sd" = function(x) { sd(x, na.rm = TRUE) },
+        "Var" = function(x) { var(x, na.rm = TRUE) },
+        "c95" = c95,
+        "95% CI" = function(x) { quantile(x, probs = c(0.025, 0.975), na.rm = TRUE) },
+        "99% CI" = function(x) { quantile(x, probs = c(0.005, 0.995), na.rm = TRUE) }
+      )
       withProgress(message = "Computing predictions", value = 0, {
-        pred <- try(predict(m, newdata = nd, term = input$variables_selected,
-          FUN = function(x) { x }), silent = TRUE)
+        pred <- try(predict(m, newdata = nd, term = input$terms_selected, FUN = FUN), silent = TRUE)
       })
       if(file.exists("predictions/pred.Rda"))
         file.remove("predictions/pred.Rda")
@@ -138,7 +169,7 @@ bamlss_shiny_server <- function(input, output, session)
 
   available_predictions <- reactive({
     e1 <- input$load_model
-    e2 <- input$variables_selected
+    e2 <- input$terms_selected
     e3 <- input$predict_model
     dir("predictions")
   })
@@ -149,7 +180,7 @@ bamlss_shiny_server <- function(input, output, session)
       m <- get(input$selected_model, envir = .GlobalEnv)
       nf <- family(m)$names
       rval <- tagList(
-        selectInput('plot_type', 'Select the type of plot.', c("Histogram")),
+        selectInput('plot_type', 'Select the type of plot.', c("Histogram", "Effect"), selected = "Histogram"),
         if(length(nf) > 1) {
           selectInput('selected_parameters', 'Select distribution parameter.',
             nf, selected = nf[1], multiple = TRUE, selectize = TRUE)
@@ -162,7 +193,7 @@ bamlss_shiny_server <- function(input, output, session)
 
   output$plot_parameters <- renderUI({
     rval <- NULL
-    if(length(available_predictions())) {
+    if(length(available_predictions()) & length(input$plot_type)) {
       if(input$plot_type == "Histogram") {
         rval <- tagList(numericInput("nbreaks", "Number of breaks.", -1, min = 1, max = 1000, step = 1))
       }
@@ -187,9 +218,14 @@ bamlss_shiny_server <- function(input, output, session)
           if(length(nf) > 1)
             nf <- input$selected_parameters
           for(i in nf) {
-            hist(pred[[i]], breaks = if(input$nbreaks < 0) "Sturges" else input$nbreaks, freq = FALSE,
-              main = i, xlab = "Predictions")
-            lines(density(pred[[i]]))
+            if(input$plot_type == "Histogram") {
+              hist(pred[[i]], breaks = if(input$nbreaks < 0) "Sturges" else input$nbreaks,
+                freq = FALSE, main = i, xlab = "Predictions")
+              lines(density(pred[[i]]))
+            }
+            if(input$plot_type == "Effect") {
+            
+            }
           }
         }
       })
