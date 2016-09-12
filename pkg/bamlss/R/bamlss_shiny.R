2aeb7dbcb81fbe250b3079c66713406d5a5fe497
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-12 15:09:34 +0000

    more fun with shiny
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@2012 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/R/bamlss_shiny.R
+++ pkg/bamlss/R/bamlss_shiny.R
@@ -25,17 +25,17 @@ bamlss_shiny_ui <- function(...) {
          uiOutput("select_model"),
          actionButton("load_model", label = "Load model."),
          tags$hr(),
+         uiOutput("select_intercept"),
          uiOutput("select_terms"),
          uiOutput("data_select")
        )
      ),
      ## Show a plot of the generated distribution.
      mainPanel(
-       uiOutput("predict"),
-       uiOutput("plot_type_button"),
-       uiOutput("plot_parameters"),
-       uiOutput("plot_button"),
-       plotOutput("the_plot", height = "800px", width = "600px"),
+       uiOutput("predict_parameters"),
+       uiOutput("set_plot"),
+       plotOutput("make_plot"),
+       tags$br(),
        tags$br()
      )
    ))
@@ -79,6 +79,9 @@ bamlss_shiny_server <- function(input, output, session)
         bamlss:::all.labels.formula(x$formula)
       })
       tn <- unique(do.call("c", tn))
+      output$select_intercept <- renderUI({
+        selectInput("intercept", "Include intercept?", c("yes", "no"), selected = "no")
+      })
       output$select_terms <- renderUI({
         selectInput('terms_selected', 'Select terms for visualization.',
           tn, multiple = TRUE, selectize = TRUE)
@@ -99,35 +102,53 @@ bamlss_shiny_server <- function(input, output, session)
               x <- pretty(mf[[i]])
               return(sliderInput(vn, i, min = min(x), max = max(x), value = range(x)))
             } else {
-              return(selectInput(vn, i, levels(mf[[i]])))
+              return(selectInput(vn, i, levels(mf[[i]]), multiple = TRUE, selectize = TRUE, selected = levels(mf[[i]])[1]))
             }
           })
-          rval <- do.call(tagList, c(list(tags$b("Choose data range.")), sliders))
+          rval <- do.call(tagList, c(list(tags$b("Choose data range/levels.")), sliders))
         } else rval <- NULL
         rval
       })
     }
   })
 
-  output$predict <- renderUI({
-    if(!is.null(input$terms_selected)) {
+  output$predict_parameters <- renderUI({
+    if(!is.null(input$selected_model)) {
+      m <- get(input$selected_model, envir = .GlobalEnv)
+      nf <- family(m)$names
+      nsamps <- nrow(m$samples[[1]]) - 1L
       rval <- tagList(
         numericInput("ngrid", "Size of grid.", 30, min = 1, max = 1000),
+        numericInput("nsamps", "Number of samples.", nsamps, min = 1, max = nsamps),
         selectInput('predict_fun', 'Select function applied on samples.',
           c("None", "Mean", "Median", "Sd", "Var", "95% CI", "99% CI", "c95"), selected = "c95"),
-        actionButton("predict_model", label = "Predict."),
+        if(length(nf) > 1) {
+          selectInput('selected_parameters', 'Select distribution parameter.',
+            nf, selected = nf[1], multiple = TRUE, selectize = TRUE)
+        } else NULL,
+        selectInput('plot_type', 'Select the type of plot.', c("Histogram", "Effect"), selected = "Histogram"),
+        conditionalPanel(condition = "input.plot_type == 'Histogram'",
+          numericInput("nbreaks", "Number of breaks.", -1, min = 1, max = 1000, step = 1)),
         tags$hr()
       )
     } else rval <- NULL
     rval
   })
 
-  observeEvent(input$predict_model, {
-    if(!is.null(input$terms_selected)) {
+  model_terms <- reactive({
+    e1 <- input$intercept
+    input$terms_selected
+  })
+
+  newdata <- reactive({
+    nd <- NULL
+    if(!is.null(input$intercept))
+      nd <- input$intercept
+    if(!is.null(model_terms()) & !is.null(input$ngrid)) {
       m <- get(input$selected_model, envir = .GlobalEnv)
       mf <- na.omit(model.frame(m))
       vars <- NULL
-      for(j in input$terms_selected)
+      for(j in model_terms())
         vars <- c(vars, all.vars(as.formula(paste("~", j))))
       vars <- unique(vars)
       for(j in seq_along(vars)) {
@@ -135,15 +156,28 @@ bamlss_shiny_server <- function(input, output, session)
           vars[j] <- grep(vars[j], names(mf), fixed = TRUE, value = TRUE)
       }
       nd <- list()
+      all_NULL <- FALSE
       for(i in vars) {
         vn <- paste("var_", i, sep = "")
-        if(is.character(input[[vn]])) {
-          nd[[i]] <- factor(input[[vn]], levels = levels(mf[[i]]))
+        if(!is.null(input[[vn]])) {
+          if(is.character(input[[vn]])) {
+            nd[[i]] <- factor(input[[vn]], levels = levels(mf[[i]]))
+          } else {
+            nd[[i]] <- seq(input[[vn]][1], input[[vn]][2], length = as.integer(input$ngrid))
+          }
+          all_NULL <- c(all_NULL, FALSE)
         } else {
-          nd[[i]] <- seq(input[[vn]][1], input[[vn]][2], length = as.integer(input$ngrid))
+          all_NULL <- c(all_NULL, TRUE)
         }
       }
-      nd <- expand.grid(nd)
+      nd <- if(!all(all_NULL)) expand.grid(nd) else NULL
+    }
+    nd
+  })
+
+  predict_model <- reactive({
+    if(!is.null(nd <- newdata()) & !is.null(input$predict_fun)) {
+      m <- get(input$selected_model, envir = .GlobalEnv)
       FUN <- switch(input$predict_fun,
         "None" = function(x) { x },
         "Mean" = function(x) { mean(x, na.rm = TRUE) },
@@ -154,115 +188,81 @@ bamlss_shiny_server <- function(input, output, session)
         "95% CI" = function(x) { quantile(x, probs = c(0.025, 0.975), na.rm = TRUE) },
         "99% CI" = function(x) { quantile(x, probs = c(0.005, 0.995), na.rm = TRUE) }
       )
-      withProgress(message = "Computing predictions", value = 0, {
-        pred <- try(predict(m, newdata = nd, term = input$terms_selected, FUN = FUN), silent = TRUE)
-      })
-      if(file.exists("predictions/pred.Rda"))
-        file.remove("predictions/pred.Rda")
-      if(!inherits(pred, "try-error")) {
-        save(pred, file = "predictions/pred.Rda")
+      if(is.character(nd)) {
+        nd <- NULL
       } else {
-        warning("Cannot compute prediction, check selected covariates!")
+        if(nrow(nd) < 1)
+          nd <- NULL
       }
-    }
-  })
-
-  available_predictions <- reactive({
-    e1 <- input$load_model
-    e2 <- input$terms_selected
-    e3 <- input$predict_model
-    dir("predictions")
-  })
-
-  output$plot_type_button <- renderUI({
-    rval <- NULL
-    if(length(available_predictions())) {
-      m <- get(input$selected_model, envir = .GlobalEnv)
-      nf <- family(m)$names
-      rval <- tagList(
-        selectInput('plot_type', 'Select the type of plot.', c("Histogram", "Effect"), selected = "Histogram"),
-        if(length(nf) > 1) {
-          selectInput('selected_parameters', 'Select distribution parameter.',
-            nf, selected = nf[1], multiple = TRUE, selectize = TRUE)
-        } else NULL,
-        tags$hr()
-      )
-    }
-    rval
-  })
-
-  output$plot_parameters <- renderUI({
-    rval <- NULL
-    if(length(available_predictions()) & length(input$plot_type)) {
-      if(input$plot_type == "Histogram") {
-        rval <- tagList(numericInput("nbreaks", "Number of breaks.", -1, min = 1, max = 1000, step = 1))
+      if(is.null(model_terms()) & input$intercept == "no") {
+        pred <- NULL
+      } else {
+        withProgress(message = "Generating plot.", value = 0, {
+          pred <- try(predict(m, newdata = nd, term = model_terms(),
+            FUN = FUN, intercept = input$intercept == "yes", nsamps = input$nsamps), silent = TRUE)
+        })
+        if(inherits(pred, "try-error"))
+          pred <- NULL
       }
-    }
-    rval
+    } else pred <- NULL
+    pred
   })
 
-  output$plot_button <- renderUI({
-    if(length(available_predictions())) {
-      rval <- actionButton("plot_predictions", label = "Plot.")
-    } else rval <- NULL
-    rval
-  })
-
-  observeEvent(input$plot_predictions, {
-    if(length(available_predictions())) {
-      output$the_plot <- renderPlot({
-        if(length(available_predictions()) & file.exists("predictions/pred.Rda")) {
-          load("predictions/pred.Rda")
-          par(mfrow = n2mfrow(length(pred)), mar = c(4.1, 4.1, 4.1, 0.1))
-          nf <- names(pred)
-          if(length(nf) > 1)
-            nf <- input$selected_parameters
-          for(i in nf) {
-            if(input$plot_type == "Histogram") {
-              rdens <- density(pred[[i]])
-              rh <- hist(pred[[i]], plot = FALSE)
-              args <- list()
-              args$ylim <- c(0, max(c(rh$density, rdens$y)))
-              args$freq <- FALSE
-              args$x <- pred[[i]]
-              args$ylab <- "Density"
-              args$xlab <- "Predictions"
-              args$main <- i
-              args$breaks <- if(input$nbreaks < 0) "Sturges" else input$nbreaks
-              ok <- try(do.call("hist", args))
-              if(!inherits(ok, "try-error"))
-                lines(rdens)
-              box()
-            }
-            if(input$plot_type == "Effect") {
-              m <- get(input$selected_model, envir = .GlobalEnv)
-              mf <- na.omit(model.frame(m))
-              vars <- NULL
-              for(j in input$terms_selected)
-                vars <- c(vars, all.vars(as.formula(paste("~", j))))
-              vars <- unique(vars)
-              for(j in seq_along(vars)) {
-                if(!any(vars[j] %in% names(mf)))
-                  vars[j] <- grep(vars[j], names(mf), fixed = TRUE, value = TRUE)
-              }
-              nd <- list()
-              for(j in vars) {
-                vn <- paste("var_", j, sep = "")
-                if(is.character(input[[vn]])) {
-                  nd[[j]] <- factor(input[[vn]], levels = levels(mf[[j]]))
-                } else {
-                  nd[[j]] <- seq(input[[vn]][1], input[[vn]][2], length = as.integer(input$ngrid))
-                }
-              }
-              nd <- expand.grid(nd)
-              if(length(vars) < 2)
-                plot2d(pred[[i]] ~ nd[[vars]], xlab = vars, ylab = "Predictions")
-              if(length(vars) < 3)
-                plot3d(pred[[i]] ~ nd[[vars[1]]] + nd[[vars[2]]], xlab = vars[1], ylab = vars[2], zlab = "Predictions")
+  output$make_plot <- renderPlot({
+    if(!is.null(pred <- predict_model())) {
+      nf <- names(pred)
+      if(length(nf) > 1)
+        nf <- input$selected_parameters
+      par(mfrow = n2mfrow(length(nf)))
+      for(i in nf) {
+        if(input$plot_type == "Histogram") {
+          par(mar = c(4.1, 4.1, 4.1, 0.1))
+          rdens <- density(pred[[i]])
+          rh <- hist(pred[[i]], plot = FALSE)
+          args <- list()
+          args$ylim <- c(0, max(c(rh$density, rdens$y)))
+          args$freq <- FALSE
+          args$x <- pred[[i]]
+          args$ylab <- "Density"
+          args$xlab <- "Predictions"
+          args$main <- i
+          args$breaks <- if(input$nbreaks < 0) "Sturges" else input$nbreaks
+          ok <- try(do.call("hist", args))
+          if(!inherits(ok, "try-error"))
+            lines(rdens)
+          box()
+        }
+        if(input$plot_type == "Effect" & !is.null(model_terms())) {
+          m <- get(input$selected_model, envir = .GlobalEnv)
+          mf <- na.omit(model.frame(m))
+          vars <- NULL
+          for(j in model_terms())
+            vars <- c(vars, all.vars(as.formula(paste("~", j))))
+          vars <- unique(vars)
+          for(j in seq_along(vars)) {
+            if(!any(vars[j] %in% names(mf)))
+              vars[j] <- grep(vars[j], names(mf), fixed = TRUE, value = TRUE)
+          }
+          nd <- list()
+          for(j in vars) {
+            vn <- paste("var_", j, sep = "")
+            if(is.character(input[[vn]])) {
+              nd[[j]] <- factor(input[[vn]], levels = levels(mf[[j]]))
+            } else {
+              nd[[j]] <- seq(input[[vn]][1], input[[vn]][2], length = as.integer(input$ngrid))
             }
           }
+          nd <- expand.grid(nd)
+          if(length(vars) == 1) {
+            par(mar = c(4.1, 4.1, 4.1, 0.1))
+            plot2d(pred[[i]] ~ nd[[vars]], xlab = vars, ylab = "Predictions")
+          }
+          if(length(vars) == 2) {
+            par(mar = c(0.1, 0.1, 4.1, 0.1))
+            plot3d(pred[[i]] ~ nd[[vars[1]]] + nd[[vars[2]]], xlab = vars[1], ylab = vars[2], zlab = "Predictions")
+          }
         }
-      })
+      }
     }
   })
 }
