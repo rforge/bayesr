98211db1e16458d851630212c04d1e0fb20160fc
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-01-24 08:26:46 +0000

    lasso
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@2266 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/R/BAMLSS.R
+++ pkg/bamlss/R/BAMLSS.R
@@ -3921,7 +3921,7 @@ smooth.construct.la.smooth.spec <- function(object, data, knots, ...)
   object$S <- list()
   const <- object$xt$const
   if(is.null(const))
-    const <- 1e-08
+    const <- 1e-06
   if(!fuse) {
     if(object$type == "single") {
       object$S[[1]] <- function(parameters) {
