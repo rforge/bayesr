e6fe9ff4baaf1c2719e13c26ba35b94591236276
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-02-09 10:30:09 +0000

    missing comma in paste() -> fixed now but maybe use sprintf() instead?
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@2285 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/R/optimizers.R
+++ pkg/bamlss/R/optimizers.R
@@ -901,7 +901,7 @@ bfit <- function(x, y, family, start = NULL, weights = NULL, offset = NULL,
                 lpost1 <- family$loglik(y, family$map2par(eta2)) + lp + x[[nx[j]]]$smooth.construct[[sj]]$prior(p.state$parameters)
                 if(lpost1 < lpost0) {
                   p.state <- x[[nx[j]]]$smooth.construct[[sj]]$state
-                  warning(paste("logPost is decreasing updating term: ", nx[j], ", "
+                  warning(paste("logPost is decreasing updating term: ", nx[j], ", ",
                     x[[nx[j]]]$smooth.construct[[sj]]$label, "; not updated, diff:", lpost1 - lpost0, sep = ""))
                 }
               }
