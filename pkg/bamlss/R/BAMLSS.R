7ac1455869eb8f95009f071087b78cbeec112b06
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-05-15 15:37:48 +0000

    lasso hyper-parameters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@2457 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/R/BAMLSS.R
+++ pkg/bamlss/R/BAMLSS.R
@@ -4235,8 +4235,8 @@ smooth.construct.la.smooth.spec <- function(object, data, knots, ...)
   }
 
   object$xt[["prior"]] <- "ig"
-  object$xt[["a"]] <- 10
-  object$xt[["b"]] <- 1e-5
+  object$xt[["a"]] <- 1e-4
+  object$xt[["b"]] <- 1e-4
   object$fixed <- if(is.null(object$xt[["fx"]])) FALSE else object$xt[["fx"]]
   priors <- make.prior(object)
   object$prior <- priors$prior
