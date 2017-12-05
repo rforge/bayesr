f1434ffbe49dcc84ffa928bbfa302805f19b686b
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-12-05 12:52:30 +0000

    started check script based on rhub
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@2598 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/BayesXsrc/inst/checks.R
@@ -0,0 +1,10 @@
+library("rhub")
+
+## Validate your e-mail address.
+validate_email()
+
+## See all platforms that can be used for checking.
+platforms()
+
+## Run check with Rdevel.
+check_with_rdevel("R2BayesX_1.1-1.tar.gz")
