b614aad22b8f4b1dc969b55afb8685d7dd24003c
tsimon <tsimon@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-05-14 12:42:54 +0000

    Add model script for jss application
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@3125 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/bamlss/paper/jss-bamlss/aldis_era5_model.R
@@ -0,0 +1,43 @@
+## --- setup ---
+set.seed(111)
+
+## --- libraries ---
+library("bamlss")
+
+## --- data ---
+load("ALDIS_ERA5_subset.rda")
+
+## --- build formula ---
+p <- names(d)
+p <- p[p != "counts"]
+p <- paste0("s(",p ,", bs = 'ps')")
+p <- paste0(p, collapse = " + ")
+f <- as.formula(paste("~", p))
+f <- list(update(f, counts ~ .), f)
+
+## --- stability selection ---
+sel <- stabsel(f, data = d, family = "ztnbinom", q = 16, B = 100)
+
+pdf("stabsel.pdf")
+plot(sel, show = 25)
+dev.off()
+
+save(sel, file = "stabsel.rda")
+
+## --- fit selected model ---
+newf <- formula(sel)
+fam <- family(sel)
+
+b <- bamlss(newf, data = d, family = fam,     # standard interface
+  binning = TRUE,                             # general arguments
+  optimizer = boost, maxit = 1000,            # boosting arguments
+  thin = 5, burnin = 1000, n.iter = 6000      # sampler arguments
+)
+
+fit <- as.data.frame(predict(b, newdata = d_eval, type = "parameter"))
+d_eval <- cbind(d_eval, fit)
+
+save(b, d_eval, fam, sel, file = "full_model.rda")
+
+
+
