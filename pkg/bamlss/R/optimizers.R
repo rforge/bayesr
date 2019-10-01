0c4e52f0bb3f3356c7db0a7cee5f348343a9826a
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-10-01 10:41:28 +0000

    :
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@3497 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/R/optimizers.R
+++ pkg/bamlss/R/optimizers.R
@@ -5617,7 +5617,7 @@ bbfit <- function(x, y, family, shuffle = TRUE, start = NULL, offset = NULL,
           }
           attr(x[[i]]$smooth.construct[[j]]$S[[lS + 1]], "npar") <- ncX
         }
-        tau2[[i]][[j]] <- rep(0.001, length(x[[i]]$smooth.construct[[j]]$S))
+        tau2[[i]][[j]] <- rep(1/ncX, length(x[[i]]$smooth.construct[[j]]$S))
         if(is.null(start)) {
           beta[[i]][[paste0("s.", j)]] <- rep(0, ncX)
         } else {
