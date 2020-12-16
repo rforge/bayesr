637ec978e5b45dbdd8bb0d9672c528a8b57b34b0
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-12-16 04:26:01 +0000

    parallel stabsel
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@3789 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/R/stabsel.R
+++ pkg/bamlss/R/stabsel.R
@@ -39,20 +39,36 @@ stabsel <- function(formula, data, family = "gaussian",
         seeds <- as.integer(seed) + seq(B)
     }
 
+    cores <- list(...)$cores
+
     ## --- Stability Selection ---
-    ## TODO: parallel option
-    stabselection <- NULL
-    for (i in seq(B)) {
+    if(!is.null(cores)) {
+      parallel_fun <- function(i) {
         cat(sprintf("Stability selection boosting run %d / %d \r", i, B))
         xx <- StabStep(formula = formula, data = data,
                        family  = family, q = q, maxit = maxit, seed = seeds[i],
                        fraction = fraction, ...)
-        stabselection <- c(stabselection, xx$sel)
+        return(list("sel" = xx$sel, "formula" = xx$formula, "family" = xx$family))
+      }
+      psel <- parallel::mclapply(seq(B), parallel_fun, mc.cores = cores)
+      stabselection <- sapply(psel, function(x) { x$sel })
+      formula <- psel[[1L]]$formula
+      environment(formula) <- NULL
+      family <- psel[[1L]]$family
+    } else {
+      stabselection <- NULL
+      for (i in seq(B)) {
+          cat(sprintf("Stability selection boosting run %d / %d \r", i, B))
+          xx <- StabStep(formula = formula, data = data,
+                         family  = family, q = q, maxit = maxit, seed = seeds[i],
+                         fraction = fraction, ...)
+          stabselection <- c(stabselection, xx$sel)
+      }
+      cat("\n")
+      formula <- xx$formula
+      environment(formula) <- NULL
+      family  <- xx$family
     }
-    cat("\n")
-    formula <- xx$formula
-    environment(formula) <- NULL
-    family  <- xx$family
 
     ## --- Re-build formula ---
     tabsel <- sort(table(stabselection), decreasing = FALSE)
