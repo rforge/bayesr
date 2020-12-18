ef07874ae0c20a49e35df0f9dcf213364d95f484
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-12-18 10:33:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@3822 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/paper/jss-bamlss/R2/fatalities_models.R
+++ pkg/bamlss/paper/jss-bamlss/R2/fatalities_models.R
@@ -20,39 +20,27 @@ parallel_fun <- function(j) {
 
   fam <- get(j)
 
-  b1 <- try(bamlss(num ~ 1, data = fatalities,
-    family = fam(mu.link = "log"), optimizer = opt_boost, init = FALSE,
-    plot = FALSE, maxit = 1000, n.iter = 12000, burnin = 2000, thin = 10),
-    silent = TRUE)
+  b1 <- bamlss(num ~ 1, data = fatalities, family = fam(mu.link = "log"),
+    n.iter = 12000, burnin = 2000, thin = 10)
 
-  b2 <- try(bamlss(f, data = fatalities,
-    family = fam(mu.link = "log"), optimizer = opt_boost, init = TRUE,
-    plot = FALSE, maxit = 1000, n.iter = 12000, burnin = 2000, thin = 10),
-    silent = TRUE)
+  b2 <- bamlss(f, data = fatalities, family = fam(mu.link = "log"),
+    n.iter = 12000, burnin = 2000, thin = 10)
 
   rval <- list()
   rval$distribution <- j
 
-  if(!inherits(b1, "try-error")) {
-    par <- predict(b1, type = "parameter", drop = FALSE)
-    dic <- DIC(b1)
-    dnum <- family(b1)$d(fatalities$num, par)
-    cat(".. .. b1: DIC =", dic$DIC, "pd =", dic$pd, "\n")
-    rval$dic1 <- dic
-    rval$dnum <- dnum
-    rval$b1 <- b1
-  } else {
-    writeLines(b1)
-  }
+  par <- predict(b1, type = "parameter", drop = FALSE)
+  dic <- DIC(b1)
+  dnum <- family(b1)$d(fatalities$num, par)
+  cat(".. .. b1: DIC =", dic$DIC, "pd =", dic$pd, "\n")
+  rval$dic1 <- dic
+  rval$dnum <- dnum
+  rval$b1 <- b1
 
-  if(!inherits(b2, "try-error")) {
-    dic <- DIC(b2)
-    cat(".. .. b2: DIC =", dic$DIC, "pd =", dic$pd, "\n")
-    rval$dic2 <- dic
-    rval$b2 <- b2
-  } else {
-    writeLines(b2)
-  }
+  dic <- DIC(b2)
+  cat(".. .. b2: DIC =", dic$DIC, "pd =", dic$pd, "\n")
+  rval$dic2 <- dic
+  rval$b2 <- b2
 
   return(rval)
 }
