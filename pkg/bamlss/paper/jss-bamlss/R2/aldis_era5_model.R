3a7d8714de760abfeceaf99a569b8fb7305f3553
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-01-02 07:22:56 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@3886 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/paper/jss-bamlss/R2/aldis_era5_model.R
+++ pkg/bamlss/paper/jss-bamlss/R2/aldis_era5_model.R
@@ -21,47 +21,26 @@ f <- list(
 )
 
 ## Estimate models.
-set.seed(456)
+set.seed(123)
 b_ztnbinom <- bamlss(f, data = FlashAustriaTrain,       ## Standard interface.
   family = "ztnbinom", binning = TRUE,                  ## General arguments.
   optimizer = opt_boost, maxit = 1000,                  ## Boosting arguments.
-  thin = 50, burnin = 500, n.iter = 1500, cores = 50,   ## Sampler arguments.
+  thin = 50, burnin = 2000, n.iter = 2500, cores = 50,  ## Sampler arguments.
   light = TRUE)
 
-set.seed(456)
+set.seed(123)
 b_ztSICHEL <- bamlss(f, data = FlashAustriaTrain,
   family = ztSICHEL, binning = TRUE,
   optimizer = opt_boost, maxit = 1000,
-  thin = 50, burnin = 500, n.iter = 1500, cores = 50,
+  thin = 50, burnin = 2000, n.iter = 2500, cores = 50,
   light = TRUE)
 
-set.seed(456)
+set.seed(123)
 b_ztBNB <- bamlss(f, data = FlashAustriaTrain,
   family = ztBNB, binning = TRUE,
   optimizer = opt_boost, maxit = 1000,
-  thin = 50, burnin = 500, n.iter = 1500, cores = 50,
+  thin = 50, burnin = 2000, n.iter = 2500, cores = 50,
   light = TRUE)
 
-save(b_ztnbinom, b_ztSICHEL, b_ztBNB, file = "flashmodels.rda")
-
-### --- predictions ---
-fit1 <- predict(b_ztnbinom, newdata = FlashAustriaEval, type = "parameter")
-fit2 <- predict(b_ztSI, newdata = FlashAustriaEval, type = "parameter")
-fit3 <- predict(b_ztBNB, newdata = FlashAustriaEval, type = "parameter")
-
-family(b_ztnbinom)$loglik(FlashAustriaEval$counts, fit1)
-family(b_ztSI)$loglik(FlashAustriaEval$counts, fit2)
-family(b_ztBNB)$loglik(FlashAustriaEval$counts, fit3)
-
-e1t <- residuals(b_ztnbinom, newdata = FlashAustriaTrain)
-e2t <- residuals(b_ztSI, newdata = FlashAustriaTrain)
-e3t <- residuals(b_ztBNB, newdata = FlashAustriaTrain)
-
-e1e <- residuals(b_ztnbinom, newdata = FlashAustriaEval)
-e2e <- residuals(b_ztSI, newdata = FlashAustriaEval)
-e3e <- residuals(b_ztBNB, newdata = FlashAustriaEval)
-
-par(mfrow = c(1, 2))
-plot(c("SItr" = e1t, "ztnbinom" = e2t, "BNB" = e3t), which = "wp", main = "Training", pos = "top")
-plot(c("SItr" = e1e, "ztnbinom" = e2e, "BNB" = e3e), which = "wp", main = "Testing", pos = "top")
+save(b_ztnbinom, b_ztSICHEL, b_ztBNB, file = "FlashAustriaModel.rda")
 
