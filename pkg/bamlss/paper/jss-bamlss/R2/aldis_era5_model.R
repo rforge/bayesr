1c857273353bb742661b31ad1b484a5b6b477c8a
freezenik <freezenik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-01-28 19:31:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@3969 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bamlss/paper/jss-bamlss/R2/aldis_era5_model.R
+++ pkg/bamlss/paper/jss-bamlss/R2/aldis_era5_model.R
@@ -12,16 +12,13 @@ ztSICHEL <- trun(0, family = "SICHEL", local = FALSE)
 ## Generate zero truncated beta negative binomial.
 ztBNB <- trun(0, family = "BNB", local = FALSE)
 
-## Model formula, up to four parameters.
+## Model formula.
 f <- list(
-  counts ~ s(sqrt_cape, bs = "ps") + s(hcc, bs = "ps") + s(str, bs = "ps") +
-           s(tciw, bs = "ps") + s(q_prof_PC1, bs = "ps"),
-         ~ s(sqrt_cape, bs = "ps") + s(hcc, bs = "ps") + s(str, bs = "ps") +
-           s(tciw, bs = "ps") + s(q_prof_PC1, bs = "ps"),
-         ~ s(sqrt_cape, bs = "ps") + s(hcc, bs = "ps") + s(str, bs = "ps") +
-           s(tciw, bs = "ps") + s(q_prof_PC1, bs = "ps"),
-         ~ s(sqrt_cape, bs = "ps") + s(hcc, bs = "ps") + s(str, bs = "ps") +
-           s(tciw, bs = "ps") + s(q_prof_PC1, bs = "ps")
+  counts ~ s(d2m, bs = "ps") + s(q_prof_PC1, bs = "ps") +
+           s(cswc_prof_PC4, bs = "ps") + s(t_prof_PC1, bs = "ps") +
+           s(v_prof_PC2, bs = "ps") + s(sqrt_cape, bs = "ps"), 
+         ~ s(sqrt_lsp, bs = "ps")
+         ~ 1
 )
 
 ## Estimate models.
@@ -29,44 +26,19 @@ set.seed(123)
 flash_model_ztnbinom <- bamlss(f, data = FlashAustriaTrain, ## Standard interface.
   family = "ztnbinom", binning = TRUE,                      ## General arguments.
   optimizer = opt_boost, maxit = 1000,                      ## Boosting arguments.
-  thin = 10, burnin = 2000, n.iter = 12000,                 ## Sampler arguments.
+  thin = 3, burnin = 1000, n.iter = 3000,                 ## Sampler arguments.
   light = TRUE)
 
 set.seed(123)
 flash_model_ztSICHEL <- bamlss(f, data = FlashAustriaTrain,
   family = ztSICHEL, binning = TRUE,
   optimizer = opt_boost, maxit = 1000,
-  thin = 50, burnin = 2000, n.iter = 3000, cores = 50,
-  light = TRUE)
-
-set.seed(123)
-flash_model_ztBNB <- bamlss(f, data = FlashAustriaTrain,
-  family = ztBNB, binning = TRUE,
-  optimizer = opt_boost, maxit = 1000,
-  thin = 50, burnin = 2000, n.iter = 3000, cores = 50,
-  light = TRUE)
-
-## Reduced formula for ztnbinom model based on boosting step.
-boost_summary(flash_model_ztnbinom)
-
-fr <- list(
-  counts ~ s(sqrt_cape, bs = "ps") + s(hcc, bs = "ps") + s(str, bs = "ps") +
-           s(tciw, bs = "ps") + s(q_prof_PC1, bs = "ps"),
-         ~ s(sqrt_cape, bs = "ps")
-)
-
-set.seed(123)
-flash_model_ztnbinom_r <- bamlss(fr, data = FlashAustriaTrain,
-  family = "ztnbinom", binning = TRUE,
-  optimizer = opt_boost, maxit = 1000,
-  thin = 10, burnin = 2000, n.iter = 12000,
+  thin = 3, burnin = 1000, n.iter = 3000,
   light = TRUE)
 
 save(
-  flash_model_ztnbinom_r,
   flash_model_ztnbinom,
   flash_model_ztSICHEL,
-  flash_model_ztBNB,
   file = "FlashAustriaModel.rda"
 )
 
